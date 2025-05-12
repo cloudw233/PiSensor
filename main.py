5a7436fb5e53a041a137d86c1fa61d88fccdf5ac
cloudw233 <qrz20081004@outlook.com>
2025-05-12 19:34:44 +0800

    update

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import importlib
+
 from fastapi import FastAPI, WebSocket
 from multiprocessing import Process
+
+from contextlib import asynccontextmanager
+
+def go(module:str):
+    importlib.import_module(f"driver.{module}.run")
+    
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+
+    yield
+
+app = FastAPI()
